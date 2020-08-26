469fe70af0d563eb564693a20467fc6d8ee677b4
Oscar A Castro <oacastro2@gmail.com>
2020-08-26 20:21:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ new_numbers = [7, 8, 9]
 
 # #def square_array(numbers)
 #   #counter = 4  
-#   #while [counter = 0] do [numbers**2]
+#   #while [counter = 0] [numbers**2]
 #   counter += 1
 #     output_new_numbers 
      
